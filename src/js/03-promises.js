e0a887340c753b5257181e401922d2eeab7048f6
Mark Sakhatskyi <mark.sahaxkiy@gmail.com>
2022-12-21 09:58:45 +0200

    thid homework

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,50 +7,101 @@ let positionPromise = null;
 let intervalId = null;
 let delay = null;
 let firstDelay = null;
-const btnDisabled = () => {
-  btn.disabled = true;
-};
+let intvalTimeout = null;
 
-btn.addEventListener('click', createPromise); 
+btn.addEventListener('click', createPromise);
 
 
 function createPromise(position, delay) {
   event.preventDefault();
-
-  btnDisabled();
+  delay = inputStepDelay[0].value;
+  console.log(delay);
   firstDelay = inputFirstDelay[0].value;
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    delay = inputStepDelay[0].value;
-   
+  console.log(firstDelay);
 
+  setTimeout(() => {
     
-         intervalId = setInterval(() => {
-      positionPromise += 1;
-      let amount = inputAmount[0].value;
-     
-      if (amount == positionPromise) {
-        clearInterval(intervalId);
-        location.reload();
-      }
-      
-      const shouldResolve = Math.random() > 0.3;
-        if (shouldResolve) {
-        // Fulfill
-      
-      
-      } else {
-        console.log(positionPromise);
-        // reject
-      }
-      }, delay) 
-  }, firstDelay)})
-    return promise;
+ 
+    intervalId = setInterval(() =>{
+  for (let i = 1; i <= inputAmount[0].value; i += 1) {
+    positionPromise +=1;
+    position +=1;
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+      console.dir(positionPromise);
+    } else {
+      // Reject
+      console.dir(positionPromise);
+    }
+
+   if (inputAmount[0].value == positionPromise) {
+      clearInterval(intervalId);
+    }
+    
+  }}, delay)
+  
+ 
+   }, firstDelay);
 };
 
-// promise.then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const btnDisabled = () => {
+//   btn.disabled = true;
+// };
+
+// btn.addEventListener('click', createPromise); 
+
+
+// function createPromise(position, delay) {
+
+
+//   btnDisabled();
+//   firstDelay = inputFirstDelay[0].value;
+// return new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     delay = inputStepDelay[0].value;
+     
+//          intervalId = setInterval(() => {
+//       positionPromise += 1;
+//       let amount = inputAmount[0].value;
+     
+//       if (amount == positionPromise) {
+//         clearInterval(intervalId);
+//         location.reload();
+//       }
+      
+//       const shouldResolve = Math.random() > 0.3;
+//         if (shouldResolve) {
+//         // Fulfill
+//       console.log(position);
+      
+//       } else {
+//         console.dir(position);
+//         // reject
+//       }
+//       }, delay) 
+//   }, firstDelay)})
+//     // return promise;
+// };
+
+// // promise.then(({ position, delay }) => {
+// //     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+// //   })
+// //   .catch(({ position, delay }) => {
+// //     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+// //   });
