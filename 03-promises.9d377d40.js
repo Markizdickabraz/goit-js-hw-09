b4612bab99a8e67c3a5eb6503bb18a3d73c53f2b
Markizdickabraz <110189603+Markizdickabraz@users.noreply.github.com>
2022-12-21 07:59:18 +0000

    Deploying to gh-pages from @ Markizdickabraz/goit-js-hw-09@e0a887340c753b5257181e401922d2eeab7048f6 🚀

--- 03-promises.9d377d40.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementsByName("delay"),n=document.getElementsByName("step"),t=document.getElementsByName("amount"),l=document.querySelector("button"),o=null,u=null,a=null;l.addEventListener("click",(function(c,m){return event.preventDefault(),function(){l.disabled=!0}(),a=e[0].value,new Promise((function(e,l){setTimeout((function(){m=n[0].value,u=setInterval((function(){o+=1,t[0].value==o&&(clearInterval(u),location.reload()),Math.random()>.3||console.log(o)}),m)}),a)}))}))}();
-//# sourceMappingURL=03-promises.9d377d40.js.map
