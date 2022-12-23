98bfde546bf806ea5eb22bcb455508f38c3abfe4
Mark Sakhatskyi <mark.sahaxkiy@gmail.com>
2022-12-23 12:43:03 +0200

    3 hw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,12 +4,13 @@ const inputAmount = document.getElementsByName('amount');
 const btn = document.querySelector('button');
     
 let positionPromise = null;
-let intervalId = null;
+// let intervalId = null;
 let delay = null;
 let firstDelay = null;
 let amount = null;
 let timeoutId = null;
 let position = null;
+let stepDelay = null;
 
 const btnDisabled = () => {
   btn.disabled = true;
@@ -17,37 +18,36 @@ const btnDisabled = () => {
 
 btn.addEventListener('click', callCreatePromiseNOW);
 
+
 function callCreatePromiseNOW(event) {
   event.preventDefault();
-  delay = inputStepDelay[0].value;
   amount = inputAmount[0].value;
-  firstDelay = inputFirstDelay[0].value;
-  // console.log(firstDelay);
+  firstDelay = parseInt(inputFirstDelay[0].value);
+  stepDelay = parseInt(inputStepDelay[0].value);
+  
   timeoutId = setTimeout(() => {
-    for (let i = 1; i < amount; i += 1) {
-      setTimeout(() => {
-     
-    createPromise(2, 1500)
+  
+    for (let i = 0; i < amount; i += 1) {
+      setTimeout(function () {
+        delay += stepDelay;
+        createPromise(position, delay)
   .then(({ position, delay }) => {
     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
-        
     }, delay);
-    }
-
+}
   },firstDelay);
 }
 
 
 function createPromise(position, delay) {
-  delay = inputStepDelay[0].value;
   positionPromise += 1;
   position = positionPromise;
   let promise =  new Promise((resolve, reject) => {
-
+    // delay = delay + stepDelay;
     const shouldResolve = Math.random() > 0.3;
     if (shouldResolve) {
       // Fulfill
@@ -60,79 +60,3 @@ function createPromise(position, delay) {
   })
   return promise;
 };
-
-// promise
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-
-
-
-
-
-
-
-// function createPromise(position, delay) {
-//   // event.preventDefault();
-//   btnDisabled();
-//   firstDelay = inputFirstDelay[0].value;
-//   setTimeout(() => {
-//     delay = inputStepDelay[0].value;
-   
-     
-//          intervalId = setInterval(() => {
-//            positionPromise += 1;
-         
-//           position = positionPromise;
-//       let amount = inputAmount[0].value;
-     
-//       if (amount == positionPromise) {
-//         clearInterval(intervalId);
-//         // location.reload();
-//       }
-      
-//            const shouldResolve = Math.random() > 0.3;
-//             return new Promise((resolve, reject) => {
-//         if (shouldResolve) {
-
-//         // Fulfill
-//         resolve('sdfddf');
-//       // console.log(position);
-//       console.dir(resolve);
-      
-//       } else {
-//           reject('dfdfddf');
-//           console.log( reject);
-//         // reject
-        
-//         }
-//         //  return Promise;
-//       }) 
-      
-      
-//     }, delay)}, firstDelay)
-//     // return promise({ position, delay });
-//     // return{position,delay}
-//   }
-
-// createPromise(2, 1500)
-//   .then(({position, delay} ) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
